697fd72e0e8e10cd89bad5bcd835642e7ce00563
Werlison aguiar <werlisonaguiar83@gmail.com>
2026-03-18 17:27:59 -0400

    add .

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avata-modo-claro (3).png')
+    }
+
+    else {
+
+        img.setAttribute('src', '../assets/avata.png.png')
+    }
+
+}
